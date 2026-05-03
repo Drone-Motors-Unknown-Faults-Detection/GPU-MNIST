6912df76ad44bf86ab5183d17fc65d9db8808f0e
JW-Albert <ru04jo30801@gmail.com>
2026-05-03 10:39:06 +0800

    Update

--- build.sh
+++ build.sh
@@ -4,15 +4,18 @@ set -e
 
 clear
 
+echo "[INFO] Upgrade PIP Version."
+pip install --upgrade pip
+
 echo "[INFO] Build Virtual Python3 Env."
 python3 -m venv venv
-echo "[INFO] Venv build completed"
+echo "[INFO] Venv Build Completed"
 
-echo "[INFO] Activate the python3 venv"
+echo "[INFO] Activate the Python3 venv"
 source venv/bin/activate
 
-echo "[INFO] Install python3 required package"
+echo "[INFO] Install Python3 Required Package"
 pip install -r requirements.txt
-echo "[INFO] Install completed"
+echo "[INFO] Install Completed"
 
 deactivate
